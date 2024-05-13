b49d530f19d3dd170d0b45f139137f69a37da228
Jirlon Oliveira <jirlon_jiw@hotmail.com>
2024-05-13 11:33:34 -0300

    subtestes

--- my-project/hello_test.go
+++ my-project/hello_test.go
@@ -3,10 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("dizendo oi para as pessoas", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("diga olá mundo quando uma string vazia é fornecida", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
 }
