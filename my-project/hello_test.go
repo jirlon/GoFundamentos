589bab212108901dbac0f6abe876c201d23a16c1
Jirlon Oliveira <jirlon_jiw@hotmail.com>
2024-05-13 12:10:17 -0300

    funcao de teste

--- my-project/hello_test.go
+++ my-project/hello_test.go
@@ -1,22 +1,25 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestHello(t *testing.T) {
 	t.Run("dizendo oi para as pessoas", func(t *testing.T) {
 		got := Hello("Chris")
 		want := "Hello, Chris"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
-	t.Run("diga olá mundo quando uma string vazia é fornecida", func(t *testing.T) {
+	t.Run("string vazia tem como padrão 'world'", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, World"
-
-		if got != want {
-			t.Errorf("got %q want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	//t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
