dc39bcd0636cf3c5eab2f5ec681d4e6cdf8d455b
Jirlon Oliveira <jirlon_jiw@hotmail.com>
2024-05-14 14:11:58 -0300

    codigo-seco-de-formas

---
+++ my-project/struct-method-interface/shapes_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(10.0, 10.0)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	got := Area(12.0, 6.0)
+	want := 72.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func Perimeter(width float64, height float64) float64 {
+	return 2 * (width + height)
+}
+
+func Area(width, height float64) float64 {
+	return width * height
+}
