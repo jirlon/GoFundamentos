362c555601f348b609db61f1902f5f3b5dd1f45f
Jirlon Oliveira <jirlon_jiw@hotmail.com>
2024-05-15 17:37:34 -0300

    injecao_dependencias

---
+++ my-project/dependency_injection/id_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"testing"
+)
+
+func TestCumprimenta(t *testing.T) {
+	buffer := bytes.Buffer{}
+	Cumprimenta(&buffer, "Chris")
+
+	resultado := buffer.String()
+	esperado := "Olá, Chris"
+
+	if resultado != esperado {
+		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+	}
+}
+
+func Cumprimenta(escritor io.Writer, nome string) {
+	fmt.Fprintf(escritor, "Olá, %s", nome)
+}
