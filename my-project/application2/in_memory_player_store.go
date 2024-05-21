0b208497731ba04c03b2cddfe13e488194103b92
Jirlon Oliveira <jirlon_jiw@hotmail.com>
2024-05-21 11:55:45 -0300

    arquivos_iniciais

---
+++ my-project/application2/in_memory_player_store.go
@@ -0,0 +1,17 @@
+package main
+
+func NovoArmazenamentoDeJogadorNaMemoria() *ArmazenamentoDeJogadorNaMemoria {
+	return &ArmazenamentoDeJogadorNaMemoria{map[string]int{}}
+}
+
+type ArmazenamentoDeJogadorNaMemoria struct {
+	armazenamento map[string]int
+}
+
+func (a *ArmazenamentoDeJogadorNaMemoria) GravarVitoria(nome string) {
+	a.armazenamento[nome]++
+}
+
+func (a *ArmazenamentoDeJogadorNaMemoria) ObtemPontuacaoDoJogador(nome string) int {
+	return a.armazenamento[nome]
+}
