31845af3f15b38c7751ff8bd44bbbff086e185d5
Jirlon Oliveira <jirlon_jiw@hotmail.com>
2024-05-27 14:59:18 -0300

    refatoração

--- my-project/command_line/cmd/cli/main.go
+++ my-project/command_line/cmd/cli/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	poker "github.com/jirlon/GoFundamentos/command_line"
+)
+
+const dbFileName = "game.db.json"
 
 func main() {
+
+	armazenamento, close, err := poker.ArmazenamentoSistemaDeArquivoJogadorAPartirDeArquivo(dbFileName)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer close()
+
 	fmt.Println("Vamos jogar poker")
+	fmt.Println("Digite {Nome} venceu para registrar uma vitoria")
+
+	poker.NovoCLI(armazenamento, os.Stdin).JogarPoker()
 }
